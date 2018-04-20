a71519ef3bd7d9f8234a50e0a5c01a6d43e7f5b8
tarari <t.jimenez@escolesnuria.cat>
2018-04-20 18:44:32 +0200

    Latest changes

--- index.php
+++ index.php
@@ -7,5 +7,30 @@
     //connection gdb
     
     $gdb=DB::getInstance();
+   
+    // insert data
     
-
+    $sql="INSERT INTO users(username,rol,email,passw) values(:username,:rol,:email,:passw)";
+    echo "<h2>".$sql."</h2>";
+    $sentencia=$gdb->query($sql);
+    
+    $gdb->bind(':username','Elon');
+    $gdb->bind(':rol',2);
+    $gdb->bind(':email','elon@blog');
+    $gdb->bind(':passw','123456');
+    //execute statement
+    $res=$gdb->execute();
+    // if true, show all users
+    if($res){
+        $sql="SELECT * FROM users";
+        $gdb->query($sql);
+        if($gdb->execute()){
+           $rows=$gdb->resultSet();
+           print_r($rows);
+           }
+        }
+       
+        
+    
+   
+   
--- sys/DB.php
+++ sys/DB.php
@@ -19,12 +19,14 @@ use App\Sys\Helper;
 class DB extends \PDO{
 
     private $stmt;
-    static private $_instance=null;
+    static private $_instance;
     
     
     static function getInstance(){
         if(!(self::$_instance instanceof self)){
+            
             self::$_instance=new self();
+            
         }
         return self::$_instance;
     }
@@ -36,12 +38,62 @@ class DB extends \PDO{
         $dsn=$dbconf['driver'].':host='.$dbconf['dbhost'].';dbname='.$dbconf['dbname'];
         $usr=$dbconf['dbuser'];
         $pwd=$dbconf['dbpass'];
-        
-        parent::__construct($dsn,$usr,$pwd);
+        try{
+            parent::__construct($dsn,$usr,$pwd);
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+         
+        }
+
+
+    public function query($sql){
+        try{
+            $this->stmt=$this->prepare($sql);
+        }catch(\PDOException $e){
+            echo $e->getMessage();
+        }
     }
-
-
-    public function query(){
+    /**
+     *  Binds statement with variables
+     * @param string $param
+     * @param string $value
+     */
+    public function bind($param,$value){
+        switch(true){
+            case is_int($value):
+                $type= \PDO::PARAM_INT;
+                break;
+            case is_bool($value):
+                $type=\PDO::PARAM_BOOL;
+                break;
+            case is_null($value):
+                $type= \PDO::PARAM_NULL;
+                break;
+            default:
+                $type= \PDO::PARAM_STR;
+                break;
+        }
+        $this->stmt->bindValue($param, $value,$type);
         
-}
+    }
+    /**
+     *  Use only after query()
+     * @return boolean
+     */
+    function execute(){
+        $result=$this->stmt->execute();
+        return $result;
+    }
+    /**
+     * Use only after execute()
+     * @return array
+     */
+    function resultSet(){
+        return $this->stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+    
+    function single(){
+        return $this->stmt->fetch(\PDO::FETCH_ASSOC);
+    }
 }
